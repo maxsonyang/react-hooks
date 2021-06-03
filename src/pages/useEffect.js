6f9ee525a4afa68f8861a2203e7a48c4af55e15e
maxsonyang <maxsonyang@berkeley.edu>
2021-06-03 09:34:03 -0700

    testing this out on gh pages

--- src/pages/useEffect.js
+++ src/pages/useEffect.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'gatsby';
+import { Helmet } from 'react-helmet';
 import { useForm } from "../components/useForm";
 import { useFetch } from "../components/useFetch";
 import CleanupComponent from "../components/useEffectCleanup";
@@ -54,107 +54,112 @@ function UseStatePage() {
   }, [count])
 
   return (
-    <div className="container-fluid">
-      <div className="page-header">
-        <h1>
-          useEffect
-        </h1>
-        <div className="page-subheader">
-          Hook that's called after each render.
-          Most of these examples log something to console, so you'll want to
-          open that up.
+    <>
+    <Helmet>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
+    </Helmet>
+      <div className="container-fluid">
+        <div className="page-header">
+          <h1>
+            useEffect
+          </h1>
+          <div className="page-subheader">
+            Hook that's called after each render.
+            Most of these examples log something to console, so you'll want to
+            open that up.
+          </div>
         </div>
-      </div>
 
-      {/* Basic useEffect Example */}
-      <div className="hooks-content">
-        <h2>
-          Basic useEffect Example
-        </h2>
-        <div className="blurb">
-          useEffect is a hook that's called after each render. You can include
-          an array of variables and/or functions as a second argument so that useEffect 
-          is only called when those variables are updated. Leaving it empty means useEffect
-          will not be called on re-renders.
+        {/* Basic useEffect Example */}
+        <div className="hooks-content">
+          <h2>
+            Basic useEffect Example
+          </h2>
+          <div className="blurb">
+            useEffect is a hook that's called after each render. You can include
+            an array of variables and/or functions as a second argument so that useEffect 
+            is only called when those variables are updated. Leaving it empty means useEffect
+            will not be called on re-renders.
+          </div>
+          <form>
+            <input
+              name="firstName"
+              placeholder="First Name"
+              value={values.firstName}
+              onChange={setValues}
+            />
+            <input
+              name="lastName"
+              placeholder="Last Name"
+              value={values.lastName}
+              onChange={setValues}
+            />
+            <input
+              name="email"
+              placeholder="Email"
+              value={values.email}
+              onChange={setValues}
+            />
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={values.password}
+              onChange={setValues}
+            />
+          </form>
         </div>
-        <form>
-          <input
-            name="firstName"
-            placeholder="First Name"
-            value={values.firstName}
-            onChange={setValues}
-          />
-          <input
-            name="lastName"
-            placeholder="Last Name"
-            value={values.lastName}
-            onChange={setValues}
-          />
-          <input
-            name="email"
-            placeholder="Email"
-            value={values.email}
-            onChange={setValues}
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={values.password}
-            onChange={setValues}
-          />
-        </form>
-      </div>
 
-      {/* Clean-up example. */}
-      <div className="hooks-content">
-        <h2>
-          Clean-up/Unmount
-        </h2>
-        <div className="blurb">
-          useEffect can also return a function that handles
-          the "clean-up" logic of a component when it unmounts.
+        {/* Clean-up example. */}
+        <div className="hooks-content">
+          <h2>
+            Clean-up/Unmount
+          </h2>
+          <div className="blurb">
+            useEffect can also return a function that handles
+            the "clean-up" logic of a component when it unmounts.
+          </div>
+          <button onClick={() => setShow((show) => !show)}>
+            Toggle show
+          </button>
+          {show && <CleanupComponent />}
         </div>
-        <button onClick={() => setShow((show) => !show)}>
-          Toggle show
-        </button>
-        {show && <CleanupComponent />}
-      </div>
 
-      {/* API fetch example. */}
-      <div className="hooks-content">
-        <h2>
-          API fetch example.
-        </h2>
-        <div className="blurb">
-          useEffect can be a good spot to fetch data from APIs as well.
-          API used: <Link to="http://numbersapi.com/" target="_blank">Numbers API</Link>
+        {/* API fetch example. */}
+        <div className="hooks-content">
+          <h2>
+            API fetch example.
+          </h2>
+          <div className="blurb">
+            useEffect can be a good spot to fetch data from APIs as well.
+            API used: <a href="http://numbersapi.com/" target="_blank">Numbers API</a>
+          </div>
+          <div>
+            {loading ? 'fun fact loading...' : data }
+          </div>
+          <button onClick={() => setRandomNumber(() => getRandomInt(100))}>
+            Get a new fact!
+          </button>
         </div>
-        <div>
-          {loading ? 'fun fact loading...' : data }
-        </div>
-        <button onClick={() => setRandomNumber(() => getRandomInt(100))}>
-          Get a new fact!
-        </button>
-      </div>
 
-      {/* Local Storage Example */}
-      <div className="hooks-content">
-        <h2>
-          Local Storage Example
-        </h2>
-        <div className="blurb">
-          useEffect can also be useful for persisting data, that is updating
-          values that we'd like to persist.
+        {/* Local Storage Example */}
+        <div className="hooks-content">
+          <h2>
+            Local Storage Example
+          </h2>
+          <div className="blurb">
+            useEffect can also be useful for persisting data, that is updating
+            values that we'd like to persist.
+          </div>
+          <div>
+            The count is: {`${count}`}
+          </div>
+          <button onClick={() => setCount((c) => c + 1)}>
+            Increase it!
+          </button>
         </div>
-        <div>
-          The count is: {`${count}`}
-        </div>
-        <button onClick={() => setCount((c) => c + 1)}>
-          Increase it!
-        </button>
       </div>
-    </div>
+    </>
   )
 }
 
